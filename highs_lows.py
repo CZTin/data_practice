13d3040edcb6909a0c81b715ff39998775e2f783
ZincS <874598493@qq.com>
2017-06-02 00:26:17 +0800

    16.1.3 abstract data

--- highs_lows.py
+++ highs_lows.py
@@ -1,10 +1,17 @@
 #coding: utf-8
 import csv
 
+#从文件中读取最高温
 filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
 	reader = csv.reader(f)
 	header_row = next(reader)
 
-	for index, column_header in enumerate(header_row):
-		print(index, column_header)
+	highs = []
+	for row in reader:
+		high = int(row[1])
+		highs.append(high)
+
+print(highs)
+#	for index, column_header in enumerate(header_row):
+#		print(index, column_header)
