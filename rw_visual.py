e00dd0ee7521cb5623bdad1a2778e82615479c6f
ZincS <874598493@qq.com>
2017-06-01 22:34:44 +0800

    15.3.10 size change

--- rw_visual.py
+++ rw_visual.py
@@ -5,12 +5,15 @@ from random_walk import Randomwalk
 #只要程序处于活动状态，就不断漫步
 while True:
 	#创建一个Randomwalk实例，并将其包含的点都绘制出来
-	rw = Randomwalk()
+	rw = Randomwalk(50000)
 	rw.fill_walk()
+
+	#设置绘图窗口的尺寸
+	plt.figure(figsize=(10, 6))
 
 	#给点着色
 	point_numbers = list(range(rw.num_points))
-	plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+	plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
 
 	#突出起点和终点
 	plt.scatter(0, 0, c='green', edgecolors='none', s=100)
@@ -19,7 +22,7 @@ while True:
 	#隐藏坐标轴
 	plt.axes().get_xaxis().set_visible(False)
 	plt.axes().get_yaxis().set_visible(False)
-		
+
 	plt.show()
 	#print(range(rw.num_points))
 
