bf75cac9bd230d992703785964009fc6ea86c5eb
ZincS <874598493@qq.com>
2017-06-01 23:05:02 +0800

    15.4.6 turn result to svg

--- die_visual.py
+++ die_visual.py
@@ -1,11 +1,27 @@
+import pygal
 from die import Die
 #创建一个D6（6个面的骰子的意思）
 die = Die()
 
 #投掷几次骰子，并将结果保存在列表里
 results =[]
-for roll_num in range(100):
+for roll_num in range(1000):
 	result = die.roll()
 	results.append(result)
 
-print(results)
+#分析结果
+frequencies = []
+for value in range(1, die.num_sides+1):
+	frequency = results.count(value)
+	frequencies.append(frequency)
+
+#对结果进行可视化
+hist = pygal.Bar()
+
+hist.title = "Result of rolling one D6 1000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
