b87b841ca1103fc38b60f652e04d909f4d01e70d
ZincS <874598493@qq.com>
2017-06-03 23:24:49 +0800

    16.2.10 world map in special color

--- world_population.py
+++ world_population.py
@@ -1,6 +1,7 @@
 import json
 import pygal.maps.world
 from country_codes import get_country_code
+from pygal.style import RotateStyle as RS, LightColorizedStyle as LCS
 
 #将数据加载到一个列表中
 filename = 'population_data.json'
@@ -17,8 +18,25 @@ for pop_dict in pop_data:
 		code = get_country_code(country_name)
 		if code:
 			cc_populations[code] = population
-wm = pygal.maps.world.World()
-wm.title = 'World Population in 2010, by Country'
-wm.add('2010', cc_populations)
 
-wm.render_to_file('world_population.svg')
+#根据人口的数量将所有的国家分成三组
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+	if pop < 10000000:
+		cc_pops_1[cc] = pop
+	elif pop < 1000000000:
+		cc_pops_2[cc] = pop
+	else:
+		cc_pops_3[cc] = pop
+
+#查看每组分别包含多少个国家
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
+wm_style = RS('#336699', base_style=LCS)
+wm = pygal.maps.world.World(style=wm_style)
+wm.title = 'World Population in 2010, by Country'
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bm', cc_pops_2)
+wm.add('>1bm', cc_pops_3)
+
+wm.render_to_file('world_population_color_formated.svg')
