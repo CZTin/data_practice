b53275fb413e0204fcf75b691c24d17374847bee
ZincS <874598493@qq.com>
2017-06-03 12:11:06 +0800

    16.1.9 fill color between high and low

--- highs_lows.py
+++ highs_lows.py
@@ -1,17 +1,39 @@
 #coding: utf-8
 import csv
-
-#从文件中读取最高温
-filename = 'sitka_weather_07-2014.csv'
+from matplotlib import pyplot as plt
+from datetime import datetime
+#从文件中读取日期、最高气温、最低气温
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
 	reader = csv.reader(f)
 	header_row = next(reader)
 
-	highs = []
+	dates, highs, lows = [], [], []
 	for row in reader:
+		current_date = datetime.strptime(row[0], "%Y-%m-%d")
+		dates.append(current_date)
+
 		high = int(row[1])
 		highs.append(high)
 
-print(highs)
+		low = int(row[3])
+		lows.append(low)
+
+#根据数据绘制图形
+fig = plt.figure(dpi=128, figsize=(6, 3))
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+#设置图形的格式
+plt.title("Daily high and low temperatures - 2014", fontsize=16)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=10)
+plt.tick_params(axis='both', which='major', labelsize=10)
+plt.show()
+
+
+#print(highs)
 #	for index, column_header in enumerate(header_row):
 #		print(index, column_header)
